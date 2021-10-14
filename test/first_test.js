16777d588becbb16d88a6f8587164e05d2ac9e78
afa7789 <afa7789@gmail.com>
2021-10-14 12:04:27 -0300

    fixing old commits from wrong author on machine + unit tests done

--- test/first_test.js
+++ test/first_test.js
@@ -1,5 +1,7 @@
 // var assert = require('assert');
 import assert from 'assert'
+import {LogicState} from '../src/rover.js'
+
 describe('Array', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
@@ -7,3 +9,136 @@ describe('Array', function() {
         });
     });
 });
+
+describe('Test LogicState Validation',function(){
+    describe('#Sucessfull Validation',function(){
+        it('should return true', function() {
+            let payload = {
+                width:5,
+                height:5,
+                rover1:{
+                    x:1,
+                    y:2,
+                    cardinal:"N",
+                    commands:"LRLRLRLRLRLMM"
+                },
+                rover2:{
+                    x:4,
+                    y:4,
+                    cardinal:"N",
+                    commands:"LRLRLRLRLRLMM"
+                }
+            }
+            let ls = new LogicState(payload)
+            assert.equal(ls.validate_parse(payload),true)
+        })
+    })
+
+    describe('#Failing Validation',function(){
+
+        it('fails validation for wrong width', function() {
+            let payload = {
+                width:"TR",
+                height:5,
+                rover1:{
+                    x:1,
+                    y:2,
+                    cardinal:"N",
+                    commands:"LRLRLRLRLRLMM"
+                },
+                rover2:{
+                    x:4,
+                    y:4,
+                    cardinal:"N",
+                    commands:"LRLRLRLRLRLMM"
+                }
+            }
+            let ls = new LogicState(payload)
+            assert.equal(ls.validate_parse(payload),false)
+        })
+
+        it('fails validation for wrong height', function() {
+            let payload = {
+                width:5,
+                height:"TR",
+                rover1:{
+                    x:1,
+                    y:2,
+                    cardinal:"N",
+                    commands:"LRLRLRLRLRLMM"
+                },
+                rover2:{
+                    x:4,
+                    y:4,
+                    cardinal:"N",
+                    commands:"LRLRLRLRLRLMM"
+                }
+            }
+            let ls = new LogicState(payload)
+            assert.equal(ls.validate_parse(payload),false)
+        })
+
+        it('fails validation for wrong position x', function() {
+            let payload = {
+                width:5,
+                height:5,
+                rover1:{
+                    x:"rr",
+                    y:2,
+                    cardinal:"N",
+                    commands:"LRLRLRLRLRLMM"
+                },
+                rover2:{
+                    x:4,
+                    y:4,
+                    cardinal:"N",
+                    commands:"LRLRLRLRLRLMM"
+                }
+            }
+            let ls = new LogicState(payload)
+            assert.equal(ls.validate_parse(payload),false)
+        })
+
+        it('fails validation for wrong position y', function() {
+            let payload = {
+                width:5,
+                height:5,
+                rover1:{
+                    x:2,
+                    y:"rr",
+                    cardinal:"N",
+                    commands:"LRLRLRLRLRLMM"
+                },
+                rover2:{
+                    x:4,
+                    y:4,
+                    cardinal:"N",
+                    commands:"LRLRLRLRLRLMM"
+                }
+            }
+            let ls = new LogicState(payload)
+            assert.equal(ls.validate_parse(payload),false)
+        })
+
+        it('fails validation for wrong cardinal', function() {
+            let payload = {
+                width:5,
+                height:5,
+                rover1:{
+                    x:1,
+                    y:2,
+                    cardinal:"T",
+                    commands:"LRLRLRLRLRLMM"
+                },
+                rover2:{
+                    x:4,
+                    y:4,
+                    cardinal:"N",
+                    commands:"LRLRLRLRLRLMM"
+                }
+            }
+            let ls = new LogicState(payload)
+            assert.equal(ls.validate_parse(payload),false)
+        })
+    })
+})
